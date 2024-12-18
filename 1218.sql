5a57269bc20caf467e282a5e9dc35a9858be214f
XII1071 <garmen1071@gmail.com>
2024-12-18 15:48:59 +0900

    1218

--- 1218.sql
+++ 1218.sql
@@ -99,7 +99,7 @@ SELECT
     *
 FROM
     USER_IND_COLUMNS; -- 인덱스 걸린 컬럼의 정보
--- 인덱스를 사용했다고 무조건 검색 효율아 좋아지는 것은 아니다.
+-- 인덱스를 사용했다고 무조건 검색 효율이 좋아지는 것은 아니다.
 -- 데이터 구조, 데이터 분포도, 조회하는 쿼리, 작업 빈도 등등 여러가지를 고려해야함.
 
 -- 2-2. 인덱스 삭제 (DROP)
@@ -139,7 +139,7 @@ FROM
 CREATE VIEW VW_EMP20
 AS 
     (   
-        SELECT     D
+        SELECT     
         EMPNO,
         ENAME,
         JOB,
@@ -170,7 +170,7 @@ DROP VIEW VW_EMP20;
 -- 3-3. 뷰에는 UPDATE, DELETE 사용 가능할까?
 --      WITH READ ONLY 가 아닌 이상 가능하다.
 --      VIEW 에서 데이터를 변경하면 원 테이블에서도 변경이된다.
--- 
+--      데이터를 변경하거나, VIEW를 생성할 때의 옵션을 잘 고려해야한다.
 
 CREATE VIEW VW_EMP20
 AS 
@@ -290,7 +290,7 @@ FROM
     DEPT_SEQUENCE;
 
 -- 시퀀스 수정
--- 기본구문 : ALTER SEQUENCE 시퀀스이름름
+-- 기본구문 : ALTER SEQUENCE 시퀀스이름
 
 --              [INCREMENT BY n]         : 증가폭 얼마만큼 증가할 것인가
 --              [MAXVALUE n / NOMAXVALUE]: 최대 값
@@ -317,3 +317,133 @@ DROP SEQUENCE SEQ_DEPT_SEQUENCE;
 -- 4-4. 시퀀스 데이터 사전 조회 ( USER_SEQUENCES )
 
 SELECT * FROM USER_SEQUENCES;
+
+-- 5. 동의어 (SYNONYM) : 테이블, 뷰, 시퀀스 등의 객체 이름을 다른 이름으로 부여
+--          - 주로 이름이 너무 길어서 불편할 때 사용.
+-- 5-1. 동의어 생성
+-- 기본구문 : CREATE [PUBLIC] SYNONYM 동의어이름
+--              FOR [사용자.][객체이름];
+--              예제) HR계정의 EMPLOYEES 라는 객체(테이블블)를 가져올 때,
+--                  FOR HR.EMPLOYEES 라고 작성
+CREATE SYNONYM E FOR EMP;
+SELECT
+    *
+FROM
+    E;
+
+-- 5-2. 동의어 삭제
+-- 기본구문 : DROP SYNONYM 동의어 이름;
+DROP SYNONYM E;
+
+-- 5-3. 데이터 사전 조회
+SELECT
+    *
+FROM
+    USER_SYNONYMS;
+
+-- 13장 1번문제
+
+-- 1-1 EMP 테이블과 같은 구조와 데이터를 가진 EMP IDX 테이블을 만들어보세요.
+-- 1-2. 생성한 EMPIDX 테이블의 EMPNO 열에 IDX_EMPIDX_EMPNO 인덱스를 만들어보세요.
+-- 1-3. 마지막으로 인덱스가 잘 생성되었는지, 데이터 사전 뷰를 통해 확인해보세요.
+
+CREATE TABLE EMPIDX AS
+    SELECT
+        *
+    FROM
+        EMP;
+CREATE INDEX IDX_EMPIDX_EMPNO ON EMPIDX(EMPNO);
+
+SELECT
+    *
+FROM
+    USER_INDEXES
+WHERE
+    INDEX_NAME = 'IDX_EMPIDX_EMPNO';
+SELECT * FROM USER_IND_COLUMNS WHERE INDEX_NAME = 'IDX_EMPIDX_EMPNO';
+
+-- 13장 2번문제
+
+-- 2-1 EMPIDX 테이블의 데이터 중 급여(SAL)가 1500 초과인 사원들만 출력하는
+--  EMPIDX_OVER15K 뷰를 생성하세요
+-- 2-2 이름이 같은 뷰가 이미 존재할 때는 새로운 내용으로 대체 가능해야하며,
+-- 2-3 EMPIDX_OVER15K 뷰는 사원번호(EMPNO), 사원이름(ENAME), 직책(JOB)
+-- 부서번호(DEPTNO), 급여(SAL), 추가수당(COMM) 열을 가진다.
+-- 2-4 추가수당 열의 경우, 데이터를 가지고 있으면 'O' 아니면 'X'를 출력
+
+CREATE OR REPLACE VIEW EMPIDX_OVER15K
+AS 
+    (   
+        SELECT     
+        EMPNO,
+        ENAME,
+        JOB,
+        DEPTNO,
+        SAL,
+        NVL2(COMM, 'O', 'X')AS COMM
+        FROM
+            EMPIDX
+        WHERE
+            SAL > 1500
+    );
+    SELECT
+    *
+FROM
+    EMPIDX_OVER15K;
+
+-- 13장 3번 문제
+
+-- 3-1. DEPT 테이블과 같은 열과 행 구성을 가지는 DEPTSEQ 테이블 생성
+-- 3-2. 생성한 DEPTSEQ의 테이블에 시퀀스 생성
+            시작값 : 1 / 증가폭 : 1 / 최대값 : 99 / 최소값 : 1
+            최대값에서 생성 중단 / 캐시 없음
+-- 3-3. DEPTSEQ 테이블에 아래(좌)와 같이 세개의 부서를 입력하여 아래(우)의 결과물을 나타내어라.
+
+CREATE TABLE DEPTSEQ AS
+    SELECT
+        *
+    FROM
+        DEPT;
+
+CREATE SEQUENCE SEQ_DEPTSEQ
+    INCREMENT BY 1
+    START WITH 1
+    MAXVALUE 99
+    MINVALUE 1
+    NOCYCLE
+    NOCACHE;
+
+INSERT INTO DEPTSEQ(DEPTNO, DNAME, LOC)
+VALUES(SEQ_DEPTSEQ.NEXTVAL, 'DATABASE', 'SEOUL');
+INSERT INTO DEPTSEQ(DEPTNO, DNAME, LOC)
+VALUES(SEQ_DEPTSEQ.NEXTVAL, 'WEB', 'BUSAN');
+INSERT INTO DEPTSEQ(DEPTNO, DNAME, LOC)
+VALUES(SEQ_DEPTSEQ.NEXTVAL, 'MOBILE', 'ILSAN');
+
+SELECT * FROM DEPTSEQ;
+
+INSERT INTO TABLE_PK(
+    LOGIN_ID,
+    LOGIN_PW,
+    TEL
+) VALUES (
+    'ID_01',
+    'PWD01',
+    '01012341234'
+);
+
+
+-- 제약조건 추가 (CREATE TABLE 시)
+-- : 컬럼명 자료형 제약조건종류
+
+-- 제약조건 추가 (CREATE TABLE 시)
+-- : 컬럼명 자료형 제약조건종류
+
+-- 제약조건 이름과 함께 넣기 (CREATE TABLE 시)
+-- : 컬럼명 자료형 CONSTRAINT 제약조건이름 제약조건종류
+
+-- 이미 있는 테이블에 제약조건 추가하기
+-- : 
+-- : ALTER TABLE 테이블명 MODIFY (컬럼명 CONSTRAINT 이름 종류)
+-- 이름수정 
+--      (ALTER TABLE 테이블명 RENAME CONSTRAINT 제약조건이름 TO 바꿀이름)
